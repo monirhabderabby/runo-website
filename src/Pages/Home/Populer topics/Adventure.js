c5451a0a58da5022eec5ab73658dfe36cc579597
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-23 23:50:07 +0600

    retrive data from server and make ready ui card structure

--- src/Pages/Home/Populer topics/Adventure.js
+++ src/Pages/Home/Populer topics/Adventure.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import BlogCard from '../../../Shared/BlogCard';
+import Loading from '../../../Shared/Loading';
 
 const Adventure = () => {
+    const type = "Adventure"
+    const {data,isLoading} = useQuery('blog', ()=> fetch(`http://localhost:5000/blog/${type}`).then(res=> res.json()))
+
+    if(isLoading){
+        return <Loading />
+    }
     return (
         <div>
-            This is Adventure page!
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            {
+                data?.map(blog=> <BlogCard key={"blog.title"} blog={blog} />)
+            }
+            </div>
         </div>
     );
 };
