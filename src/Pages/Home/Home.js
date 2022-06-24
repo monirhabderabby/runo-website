6ec397fc2674a267ff750897f1c4733880fd2389
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-24 08:32:43 +0600

    added footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,5 @@
-
 import React from 'react';
+import Footer from '../../Shared/Footer';
 import Banner from './Banner';
 import EditorsPick from './EditorsPick/EditorsPack';
 import PopulerTopics from './Populer topics/PopulerTopics';
@@ -12,6 +12,7 @@ const Home = () => {
             <PopulerTopics />
             <Quotes />
             <EditorsPick />
+            <Footer />
         </div>
     );
 };
