2fcf3730c688d8921ee98286d90f89ea08a4973a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-24 06:36:37 +0600

    data retrive for nested route

--- src/Pages/Home/Populer topics/Technology.js
+++ src/Pages/Home/Populer topics/Technology.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import BlogCard from '../../../Shared/BlogCard';
+import Loading from '../../../Shared/Loading';
 
 const Technology = () => {
+    const type = "Technology"
+    const {data,isLoading} = useQuery('technology', ()=> fetch(`http://localhost:5000/blog/${type}`).then(res=> res.json()))
+
+    if(isLoading){
+        return <Loading />
+    }
     return (
         <div>
-            This is technology!
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            {
+                data?.map(blog=> <BlogCard key={"blog.title"} blog={blog} />)
+            }
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Populer topics/Travel.js
+++ src/Pages/Home/Populer topics/Travel.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import BlogCard from '../../../Shared/BlogCard';
+import Loading from '../../../Shared/Loading';
 
 const Travel = () => {
+    const type = "Travel"
+    const {data,isLoading} = useQuery('travel', ()=> fetch(`http://localhost:5000/blog/${type}`).then(res=> res.json()))
+
+    if(isLoading){
+        return <Loading />
+    }
     return (
         <div>
-            This is travel page!
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            {
+                data?.map(blog=> <BlogCard key={"blog.title"} blog={blog} />)
+            }
+            </div>
         </div>
     );
 };
