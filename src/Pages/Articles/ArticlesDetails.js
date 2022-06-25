f4c590e638dc4d8f2a40d1d51a0603dd2047f2b2
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-25 11:03:00 +0600

    added resposive to postdetails page

--- src/Pages/Articles/ArticlesDetails.js
+++ src/Pages/Articles/ArticlesDetails.js
@@ -18,22 +18,23 @@ const ArticlesDetails = () => {
         <div>
             <div style={{
             backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url(${data?.img})`,
-            height: "50vh",
             width: "100%",
             backgroundSize: "cover",
             backgroundPosition: "center",
-        }}>
+        }}
+        className="h-[70vh] md:h-[50vh]"
+        >
             <RunoNavbar />  
 
-            <div className='flex justify-center w-1/2 mx-auto h-full flex-col '>
-            <h1 className='text-white  text-[36px] text-center mx-auto lora'>{data?.title}</h1>
+            <div className='flex justify-center w-[90%] lg:w-1/2 mx-auto h-full flex-col '>
+            <h1 className='text-white text-[20px]  lg:text-[36px] text-center mx-auto lora'>{data?.title}</h1>
             <span className='text-white text-[12px] ml-6 mt-4 text-left'>By {data?.writter}</span>
             </div>
             
 
         </div>
 
-        <section className='w-1/2 flex mx-auto mt-20'>
+        <section className='w-[95%] md:w-1/2 flex mx-auto mt-20'>
             <div className='text-[12px]'>
                 <p className='mr-4 md:mr-8 lg:mr-24 w-full'>{data?.post_date}</p>
                 <p>{data?.writter}</p>
